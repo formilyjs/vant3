c96ff86435768ea5399845d5ba74966053a4fd73
liyaoo <liyaoo@enn.cn>
2022-05-07 11:40:47 +0800

    feat: update .gitignore

--- docs/.vuepress/.temp/internal/layoutComponents.js
+++
@@ -1,6 +0,0 @@
-import { defineAsyncComponent } from 'vue'
-
-export const layoutComponents = {
-  "404": defineAsyncComponent(() => import("/Users/liyao/frontend/code/vant3/node_modules/vuepress-theme-dumi/lib/client/layouts/404.vue")),
-  "Layout": defineAsyncComponent(() => import("/Users/liyao/frontend/code/vant3/node_modules/vuepress-theme-dumi/lib/client/layouts/Layout.vue")),
-}
